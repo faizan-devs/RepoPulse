d646eea1297a7d30e36cf7cbd0cd8a775adf8f0d
Faizan Ali <alammdfaizan50@gmail.com>
2026-07-30 12:49:21 +0530

    chore: update health check route to include success status in response

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
+import { success } from 'zod';
 
 const router = Router();
 
 router.get('/', (_req, res) => {
     res.json({
+        success: 'true',
         message: 'API v1',
     });
 });
 
-router.get('/latency', (_req, res) => {
+router.get('/health', (_req, res) => {
     res.json({
+        success: 'true',
         message: '600ms last latency',
     });
 });
